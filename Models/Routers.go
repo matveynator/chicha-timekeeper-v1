691859fc49c389d8bdc91af7701aa4a7160809c1
root <matveynator@gmail.com>
2021-05-29 14:39:25 +0300

    update
    Former-commit-id: ac7ba250d7e34fc9a813572d6c975177971bf44c

--- Models/Routers.go
+++ Models/Routers.go
@@ -1,8 +1,8 @@
 package Models
 
 /**
- * Module have methods for internal routing based on Gin framework
- */
+* Module have methods for internal routing based on Gin framework
+*/
 
 import (
 	"github.com/gin-gonic/gin"
@@ -20,29 +20,29 @@ func SetupRouter() *gin.Engine {
 	v1 := r.Group("/api")
 	{
 		v1.GET("laps", GetListLaps) // Full list of laps
-        v1.GET("laps/:id", GetLap) // Get one lap details
-        v1.GET("laps/bytagid/:id", GetLapsByTagId)
-        v1.GET("laps/delete/bytagid/:id", DeleteLap)
+		v1.GET("laps/:id", GetLap) // Get one lap details
+		v1.GET("laps/bytagid/:id", GetLapsByTagId)
+		v1.GET("laps/delete/bytagid/:id", DeleteLap)
 
-        v1.GET("races", GetListRaces)
-        v1.GET("races/:id", GetRace)
-        v1.POST("races", CreateRace)
-        v1.PUT("races/:id", UpdateRace)
-        v1.DELETE("races/:id", DeleteRace)
-        v1.POST("races/start/:id", StartRace)
-        v1.POST("races/finish", FinishRace)
+		v1.GET("races", GetListRaces)
+		v1.GET("races/:id", GetRace)
+		v1.POST("races", CreateRace)
+		v1.PUT("races/:id", UpdateRace)
+		v1.DELETE("races/:id", DeleteRace)
+		v1.POST("races/start/:id", StartRace)
+		v1.POST("races/finish", FinishRace)
 
-        v1.GET("users", GetListUsers)
-        v1.GET("users/:id", GetUser)
-        v1.POST("users", CreateUser)
-        v1.PUT("users/:id", UpdateUser)
-        v1.DELETE("users/:id", DeleteUser)
+		v1.GET("users", GetListUsers)
+		v1.GET("users/:id", GetUser)
+		v1.POST("users", CreateUser)
+		v1.PUT("users/:id", UpdateUser)
+		v1.DELETE("users/:id", DeleteUser)
 
-        v1.GET("checkins", GetListCheckins)
-        v1.GET("checkins/:id", GetCheckin)
-        v1.POST("checkins", CreateCheckin)
-        v1.PUT("checkins/:id", UpdateCheckin)
-        v1.DELETE("checkins/:id", DeleteCheckin)
+		v1.GET("checkins", GetListCheckins)
+		v1.GET("checkins/:id", GetCheckin)
+		v1.POST("checkins", CreateCheckin)
+		v1.PUT("checkins/:id", UpdateCheckin)
+		v1.DELETE("checkins/:id", DeleteCheckin)
 
 	}
 
