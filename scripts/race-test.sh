cb725cd786db34ffc8b645bd9c9d7e4711fc1ef0
root <matveynator@gmail.com>
2021-09-07 14:17:31 +0300

    cleanup

--- scripts/race-test.sh
+++ scripts/race-test.sh
@@ -2,9 +2,10 @@
 
 port=5000
 host=localhost
-competitors=8
-laps=5
-minimal_lap_time_sec=45
+competitors=3
+laps=2
+minimal_lap_time_sec=15
+xml=1
 
 LANG=C
 cmdname=`basename $0`
@@ -23,15 +24,18 @@ trap 'cleanup' SIGTERM
 function raceXML() {
 for lap in `seq 1 ${laps}`;
 do
-
+[ "${lap}" != "1" ] && echo
 echo "LAP #${lap}"
 
 for racer in `seq 1 ${competitors}`; 
 do
 antenna=`shuf -i 1-4 -n 1`
 time=`date +"%Y/%m/%d %T.%3N"`
+unixtime=`date +%s%3N`
 sleep_time=`shuf -i 1-15 -n 1`
 
+if [ "${xml}" == "1" ]
+then
 cat > ${spool}  <<EOF
 <Alien-RFID-Tag>
   <TagID>1000 0802 0200 0001 0000 079${racer}</TagID>
@@ -42,8 +46,13 @@ cat > ${spool}  <<EOF
   <Protocol>2</Protocol>
 </Alien-RFID-Tag>
 EOF
+else
+cat > ${spool}  <<EOF
+10000802020000010000079${racer}, ${unixtime}, ${antenna}
+EOF
+fi
 
-echo
+[ "${racer}" != "1" ] && echo
 cat ${spool}
 cat ${spool} | nc -q 0 ${host} ${port}
 
